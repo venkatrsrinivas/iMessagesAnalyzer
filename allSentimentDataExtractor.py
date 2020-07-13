95dc9d1ffcfd20b0392d1ca6bc369f5a47ec0f7a
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-07-13 15:41:36 -0400

    Implementation For Storing Only Newly Unseen Text Messages.

--- allSentimentDataExtractor.py
+++ allSentimentDataExtractor.py
@@ -8,28 +8,39 @@ Har Har Mahadev
 import sys
 import csv
 import pandas
+from datetime import datetime
 
 #Based On Input File Name,
 #Stores All iMessages Sent From User.
 #Solely Dependent On Formatted CSV File.
-def getAllSentMessages(inputFileName):
+def getAllSentMessages(inputFileName, startTimeStamp):
 	#Open CSV File. 
 	#currentFileReader = open(inputFileName, "r")
 	allDesiredColumnValues = ['text', 'timestamp','is_sent', 'phone_number']
 	allExtractData = pandas.read_csv(inputFileName, usecols=allDesiredColumnValues)
 	allSentMessages = []
-	#Loop Through Rows From CSV File.
+	#Helper Variable To Maintain Prev Time Stamp While Looping.
 	prevTimeStamp = None
+	#Loop Through Rows From CSV File.
 	for currentData in allExtractData.iterrows():
 		#Extract Data From Pandas Data-Frame:
 		currentRowValue, (currentTextValue, currentTimeStamp, currentIsSent, currentPhoneNumber) = currentData
-		#Comparing TimeStamps To Print Messages In Chronological Order Only:
-		# if(prevTimeStamp == None or currentTimeStamp >= prevTimeStamp):
-		# 	print(currentTimeStamp)
-		# else:
-		# 	prevTimeStamp = currentTimeStamp
-		if(currentIsSent):
-			formatSentMessages.append((currentTimeStamp, currentTextValue))
+		#Error-Check For Current Time Stamp As Empty + Not Proper String:
+		if(not(isinstance(currentTimeStamp, str))):
+			#Simply Set Current Time Stamp = Prev Time Stamp,
+			#Which May Possibly Be None.s
+			currentTimeStamp = prevTimeStamp;	
+		else:
+			currentTimeStamp = datetime.strptime(currentTimeStamp, '%Y-%m-%d %H:%M:%S')
+		#Comparing TimeStamps To Store Only Messages >= startTimeStamp:
+		if(startTimeStamp == None 
+			or currentTimeStamp == None 
+			or currentTimeStamp >= startTimeStamp):
+			#Assert Message Is Sent.
+			if(currentIsSent):
+				allSentMessages.append((currentTimeStamp, currentTextValue))
+		prevTimeStamp = currentTimeStamp;
+	print(len(allSentMessages))
 	return allSentMessages
 
 #Main Function For Running Sentiment Analysis,
@@ -39,8 +50,10 @@ def runSentimentAnalysisAlgorithm(allSentMessages):
 	return
 
 #For Testing Purposes Only:
-def main():
-	getAllSentMessages("/Users/vsrinivas321/Documents/VSR_iMessages_Data.csv")
+def main(inputFileName, prevLogDateTime):
+	if(prevLogDateTime != None):
+		prevLogDateTime = datetime.strptime(prevLogDateTime, '%Y-%m-%d %H:%M:%S')
+	getAllSentMessages(inputFileName, prevLogDateTime)
 
 if __name__ == '__main__':
-	main()
+	main("/Users/vsrinivas321/Documents/VSR_iMessages_Data.csv", "2020-06-20 12:53:20")
