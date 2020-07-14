1431a8f7261e5b186583b9e6fe5e0c99b50d3350
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-07-14 15:49:45 -0400

    Initial Testing For Sentiment Analysis Functionality.

--- allSentimentDataExtractor.py
+++ allSentimentDataExtractor.py
@@ -9,15 +9,18 @@ import sys
 import csv
 import pandas
 from datetime import datetime
+from textblob import TextBlob
+import emoji
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 #Based On Input File Name,
 #Stores All iMessages Sent From User.
 #Solely Dependent On Formatted CSV File.
-def getAllSentMessages(inputFileName, startTimeStamp):
+def getAllSentMessages(inputFilePath, startTimeStamp):
 	#Open CSV File. 
 	#currentFileReader = open(inputFileName, "r")
 	allDesiredColumnValues = ['text', 'timestamp','is_sent', 'phone_number']
-	allExtractData = pandas.read_csv(inputFileName, usecols=allDesiredColumnValues)
+	allExtractData = pandas.read_csv(inputFilePath, usecols=allDesiredColumnValues)
 	allSentMessages = []
 	#Helper Variable To Maintain Prev Time Stamp While Looping.
 	prevTimeStamp = None
@@ -41,6 +44,7 @@ def getAllSentMessages(inputFileName, startTimeStamp):
 				allSentMessages.append((currentTimeStamp, currentTextValue))
 		prevTimeStamp = currentTimeStamp;
 	#Return All Sent Messages.
+	print(allSentMessages)
 	return allSentMessages
 
 #Main Function For Running Sentiment Analysis,
@@ -48,15 +52,28 @@ def getAllSentMessages(inputFileName, startTimeStamp):
 #From iMessagesDataExtractor.py Extractor.
 def runSentimentAnalysisAlgorithm(allSentMessages):
 	#Setup For Tensor Flow + Machine Learning Algorithm.
-	return
+	#In-Case No Messages Were Sent In Desired Time Frame.
+	if(len(allSentMessages) == 0):
+		return [];
+	#Threshold For Negative-Text Message Classification = 0.5 Probability.
+	#Return Pairing (CurrentText, Sentiment Classification).
+	#Initialize Vader Sentiment Analyzer:
+	currentAnalyzer = SentimentIntensityAnalyzer()
+	for currentMessage in allSentMessages:
+		#Replace In Case For Emojis:
+		currentText = emoji.demojize(currentMessage[1]).replace(":", "").replace("_", " ")
+		#Run Text Blob Sentiment Analyzer:
+		currentSentiment = TextBlob(currentText).sentiment
+		#Output All Sentiment Data.
+		print(currentText, currentSentiment, currentAnalyzer.polarity_scores(currentText))
 
 #For Testing Purposes Only:
-def main(inputFileName, prevLogDateTime):
+def main(inputFilePath, prevLogDateTime):
 	#Adjust Previous Log Date Time To Allow For Comparison Of Dates.
 	if(prevLogDateTime != None or type(prevLogDateTime) != datetime):
 		prevLogDateTime = datetime.strptime(prevLogDateTime, '%Y-%m-%d %H:%M:%S')
 	#Compute All Sent Messages.
-	allSentMessages = getAllSentMessages(inputFileName, prevLogDateTime)
+	allSentMessages = getAllSentMessages(inputFilePath, prevLogDateTime)
 	#Run Sentiment Analysis ML/NLP Algorithms.
 	runSentimentAnalysisAlgorithm(allSentMessages)
 	#Return Appropriate Sentiment Data Back To Caller.
