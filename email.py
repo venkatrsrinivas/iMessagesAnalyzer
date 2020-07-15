08b8652c8bd3c397e20c353867392532820ab0aa
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-07-15 12:21:43 -0400

    Renamed Files + Added Foundation For Email Structure.

---
+++ email.py
@@ -0,0 +1,16 @@
+'''
+Jai Sri Rama
+Om Namah Shivaya
+Har Har Mahadev
+'''
+
+import sys
+
+#Simple Script That Sends An Email 
+#Given Input Email Address + Input Text Blocks.
+def sendTimedEmails(inputEmailAddress, inputTextPackage):
+
+
+#Automatically Invoked Main Driver Function:
+if __name__ == '__main__':
+	sendTimedEmails("", "");
