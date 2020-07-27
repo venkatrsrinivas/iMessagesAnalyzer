ce994e7fca25caaa0ed46b3f0be78040c877a724
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-07-27 16:45:34 -0400

    Initial Fundamental Structure Complete For Registering User Data.

--- register/models.py
+++ register/models.py
@@ -3,9 +3,11 @@ from django.db import models
 #Create Your Models Here.
 class User(models.Model):
 	#Public Member Variables.
+	currentEmail = models.EmailField()
 	currentName = models.CharField(max_length=100)
-	currentChatDataBasePath = models.CharField()
-	prevLastVisitedTime = models.DateField()
+	currentPathToRead = models.TextField()
+	currentPathToWrite = models.TextField()
+	prevComputeIndex = models.IntegerField()
 
 	#Plural Meta Data For User Class:
 	class Meta:
@@ -13,4 +15,4 @@ class User(models.Model):
 
 	#String Representation For Users:
 	def __str__(self):
-		return self.currentName
+		return self.currentName;
