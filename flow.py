8cfcdc5cb243075bedeab268426b15f33a54c3c2
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-08-03 19:12:33 -0400

    Initial Setup For Script To Train + Test Tensor Flow Model.

---
+++ flow.py
@@ -0,0 +1,44 @@
+'''
+Jai Sri Rama
+Om Namah Shivaya
+Har Har Mahadev
+'''
+
+#TensorFlow Import Statements.
+import tensorflow_datasets as tfds
+import tensorflow as tf
+from keras.models import model_from_json
+
+def runTrainTestSave(dataset, info):
+	train_dataset, test_dataset = dataset['train'], dataset['test']
+	encoder = info.features['text'].encoder
+	BUFFER_SIZE = 10000
+	BATCH_SIZE = 64
+	train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+	train_dataset = train_dataset.padded_batch(BATCH_SIZE)
+	test_dataset = test_dataset.padded_batch(BATCH_SIZE)
+	model = tf.keras.Sequential([
+		tf.keras.layers.Embedding(encoder.vocab_size, 64),
+		tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+		tf.keras.layers.Dense(64, activation='relu'),
+		tf.keras.layers.Dense(1)    
+	])
+	model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+			  optimizer=tf.keras.optimizers.Adam(1e-4),
+			  metrics=['accuracy'])
+	history = model.fit(train_dataset, epochs=10,
+					validation_data=test_dataset, 
+					validation_steps=30)
+	test_loss, test_acc = model.evaluate(test_dataset)
+
+	#Convert Trained Model To JSON File.
+	currentJSON = model.to_json()
+	#Write JSON File To Current Directory.
+	with open("/Users/vsrinivas321/Documents/iMessagesAnalyzer/allModelData.json", "w") as currentFile:
+	   currentFile.write(currentJSON)
+	#Save/Serialize Weights To HDF5 File.
+	model.save_weights("/Users/vsrinivas321/Documents/iMessagesAnalyzer/allWeightData.h5")
+
+if __name__ == '__main__':
+	runTrainTestSave()
+
