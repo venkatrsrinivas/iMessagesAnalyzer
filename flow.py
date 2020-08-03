4f2ae07147020c79ea6b45052ec617e2c834374c
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-08-03 19:55:32 -0400

    Added Comments + Re-Named Variables In Flow Script.

--- flow.py
+++ flow.py
@@ -5,49 +5,61 @@ Har Har Mahadev
 '''
 
 #TensorFlow Import Statements.
-import tensorflow_datasets as tfds
 import tensorflow as tf
+import tensorflow_datasets as tfds
 from keras.models import model_from_json
 
+#Main Driver Training + Testing Function.
 def runTrainTestSave():
-	# dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
-	# 						  as_supervised=True)
-	# encoder = info.features['text'].encoder
-	# train_dataset, test_dataset = dataset['train'], dataset['test']
-	# encoder = info.features['text'].encoder
-	# BUFFER_SIZE = 10000
-	# BATCH_SIZE = 64
-	# train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-	# train_dataset = train_dataset.padded_batch(BATCH_SIZE)
-	# test_dataset = test_dataset.padded_batch(BATCH_SIZE)
-	# model = tf.keras.Sequential([
-	# 	tf.keras.layers.Embedding(encoder.vocab_size, 64),
-	# 	tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-	# 	tf.keras.layers.Dense(64, activation='relu'),
-	# 	tf.keras.layers.Dense(1)    
-	# ])
-	# model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-	# 		  optimizer=tf.keras.optimizers.Adam(1e-4),
-	# 		  metrics=['accuracy'])
-	# history = model.fit(train_dataset, epochs=10,
-	# 				validation_data=test_dataset, 
-	# 				validation_steps=30)
-	# test_loss, test_acc = model.evaluate(test_dataset)
+	#Load All Movie Review Data Set + Related Information:
+	currentDataSet, currentInfoData = tfds.load('imdb_reviews/subwords8k', with_info=True,
+							  as_supervised=True)
+	#Split Data Into Train + Test Frameworks:
+	trainDataSet, testDataSet = currentDataSet['train'], currentDataSet['test']
+	currentEncoder = currentInfoData.features['text'].encoder
+	#Initialize Key Training + Testing Sample Sizes:
+	BUFFER_SIZE = 10000
+	BATCH_SIZE = 64
+	#Shuffle Train + Test Data:
+	trainDataSet = trainDataSet.shuffle(BUFFER_SIZE)
+	trainDataSet = trainDataSet.padded_batch(BATCH_SIZE)
+	testDataSet = testDataSet.padded_batch(BATCH_SIZE)
+	#Initialize Current Model:
+	currentModel = tf.keras.Sequential([
+		tf.keras.layers.Embedding(currentEncoder.vocab_size, 64),
+		tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+		tf.keras.layers.Dense(64, activation='relu'),
+		tf.keras.layers.Dense(1)    
+	])
+	#Compilation of Model:
+	currentModel.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+			  optimizer=tf.keras.optimizers.Adam(1e-4),
+			  metrics=['accuracy'])
+	#Fit Current Model Based On Trainin Data.
+	#Make Model Learn Training Data + Validate On Testing Data.
+	allHistoryData = currentModel.fit(trainDataSet, epochs=10,
+					validation_data=testDataSet, 
+					validation_steps=30)
+	#Evaluate Final Performance On Testing Data.
+	testLoss, testAccuracy = currentModel.evaluate(testDataSet)
+	#Output To Terminal Console:
+	print("Test Loss: " + str(testLoss) + " + Test Accuracy: " + str(testAccuracy))
 	#Load JSON File w/ Model Data + Create Model:
 	currentModelFile = open("allModelData.json", 'r')
 	currentModelReader = currentModelFile.read()
 	currentModelFile.close()
-	model = model_from_json(currentModelReader)
+	currentModel = model_from_json(currentModelReader)
 	#Load Model w/ Weights:
-	model.load_weights("allWeightData.h5")
+	currentModel.load_weights("allWeightData.h5")
 	#Convert Trained Model To JSON File.
-	currentJSON = model.to_json()
+	currentJSON = currentModel.to_json()
 	#Write JSON File To Current Directory.
 	with open("allModelData.json", "w") as currentFile:
 	   currentFile.write(currentJSON)
 	#Save/Serialize Weights To HDF5 File.
-	model.save_weights("allWeightData.h5")
+	currentModel.save_weights("allWeightData.h5")
 
+#Main Driver Caller/Invoker:
 if __name__ == '__main__':
 	runTrainTestSave()
 
